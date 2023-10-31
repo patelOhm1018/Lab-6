e1887ef261c8b92cde7c64c1a4cc24bb1f1972bc
Monarchman1234 <149005715+Monarchman1234@users.noreply.github.com>
2023-10-31 12:04:06 -0400

    Add files via upload

--- Lab6.py
+++ Lab6.py
@@ -7,6 +7,14 @@ def encode(password):
         encoded_password += encoded_digit
     return encoded_password
 
+#This Decode Function was written by Thomas Hahn
+def decode(encoded_password):
+    decoded_password = ""
+    for digit in encoded_password:
+        decoded_num = str((int(digit) - 3) % 10)
+        decoded_password += decoded_num
+    return decoded_password
+#This prints the menu
 def menu_display():
     print("Menu")
     print("-" * 13)
@@ -20,11 +28,13 @@ if __name__ == "__main__":
     while option:
         user_input = int(input("Please enter an option: "))
         if user_input == 1:
+            #This code is used to encode the password
             password = str(input("Please enter your password to encode: "))
             print("Your password has been encoded and stored!")
             encoded_password = encode(password)
             print(f"The encoded password is {encoded_password}, and the original password is {password}")
         if user_input == 2:
+            #This code is used to decode the password
             password = str(input("Please enter your password to decode: "))
             print("Your password has been encoded and stored!")
             decoded_password = decode(password)
