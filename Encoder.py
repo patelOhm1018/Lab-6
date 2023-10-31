5bd5f77f27bcf946c621ee02d11bd92c097362ce
Monarchman1234 <149005715+Monarchman1234@users.noreply.github.com>
2023-10-31 11:54:43 -0400

    Update Encoder.py

--- Encoder.py
+++ Encoder.py
@@ -1,18 +1,11 @@
 if __name__ == '__main__':
     #My name is Thomas Hahn and I wrote this encode function
-    def encode():
-        password = int(input("Please enter your password to encode:"))
-        password += 33333333
-        print("Your password has been encoded and stored!")
-
-    #This is the change made by Om Patel
-    def decode():
-        decoded_password =""
-        for digit in password:
-            decoded_digit = str((int(digit) - 3) % 10)
-            decoded_password += decoded_digit
-        return decoded_password
-        print("Your password has been decoded and stored!")
+    def encode(password):
+        encoded_password =""
+        for element in password:
+            encoded_num = str((int(element) + 3) % 10)
+            encoded_password += encoded_num
+        return encoded_password
 
     print("Menu")
     print("-------------")
@@ -21,6 +14,6 @@ if __name__ == '__main__':
     print("3. Quit")
     user_input = input("Please enter an option:")
     if user_input == "1":
-        encode()
-    if user_input == "2":
-        decode()
+         password = str(input("Please enter your password to encode: "))
+         encoded_password = encode(password)
+         print("Your password has been encoded and stored!")
